84d52f2b94fbe8cac32ba97bf23cab0bfcc06bb6
Abraham Levitan <allevitan@gmail.com>
2015-10-22 16:36:11 -0400

    Fix lattice_tools location bug

--- viewers/standard.py
+++ viewers/standard.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-from lattice_tools import fermi_energy, bravais_lattice, reciprocal_vectors
+from solvers.lattice_tools import fermi_energy, bravais_lattice, reciprocal_vectors
 from path_tools import path_defaults
 import itertools as it
 
